6172704972f07e37825f577a5b1086f06ed8e6f8
dmachlanski <damian.machlanski@gmail.com>
2023-06-02 15:41:22 +0100

    Exp setup - further tweaks.

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-snakemake --cores all --use-singularity --configfile config/config_1.json
+snakemake --cores all --use-singularity --configfile config/config_2.json
+
+#snakemake --cores all --use-singularity --latency-wait 20 --configfile config/hyper_sim_minimal.json
+
+#snakemake --cores all --use-singularity --latency-wait 20 --configfile config/hyper_sachs.json
+
+#snakemake --cores all --use-singularity --latency-wait 20 --configfile config/hyper_syntren.json
