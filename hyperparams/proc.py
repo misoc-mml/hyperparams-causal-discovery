fccbe703a330470a6c562bafb468651e2911e51c
dmachlanski <damian.machlanski@gmail.com>
2023-06-14 18:02:53 +0100

    Analysis -- standard error of the mean.

--- hyperparams/proc.py
+++ hyperparams/proc.py
@@ -2,7 +2,6 @@ import os
 import json
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 import utils as ut
 
@@ -23,22 +22,13 @@ df = pd.read_csv(path_to_bench, index_col=0)
 
 df = ut.extract_cols(df)
 
-df_alg_rep = df.groupby(['algorithm', 'graph_p', 'graph_d', 'graph_type', 'data_sem', 'data_n', 'replicate'], as_index=False)
+df_alg_rep = df.groupby(['id', 'graph_p', 'graph_d', 'graph_type', 'data_sem', 'data_n', 'replicate'], as_index=False)
 
 for target in targets:
     # Find the best 'target' value across hyperparameters
     # (assumes lower is better)
     best = df_alg_rep.apply(lambda x: x.loc[x[target].idxmin()])
 
-    # Boxplot grouped by algs, across data seeds
-    #best.boxplot(target, 'algorithm')
-    #plt.savefig(f'{target}.pdf')
-
-    # Summary stats grouped by algs, across data seeds
-    #df_alg = best.groupby(['algorithm'], as_index=False).agg(['mean', 'std', 'min', 'max', 'median', 'sem'])
-    #df_alg = best.groupby(['algorithm', 'graph_p', 'graph_d', 'graph_type', 'data_sem', 'data_n'], as_index=False).mean()
-
-    #df_alg[target].to_csv(f'{target}.csv')
     #df_alg.to_csv(f'{target}.csv')
 
     best.to_csv('results.csv')
