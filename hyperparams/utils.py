fe66ca8ddc28971ce632a0fd4f6d454532b35613
dmachlanski <damian.machlanski@gmail.com>
2023-08-08 16:03:02 +0100

    Percentage of wins.

--- hyperparams/utils.py
+++ hyperparams/utils.py
@@ -1,3 +1,7 @@
+import os
+import json
+import pandas as pd
+
 def extract_cols(df):
     # adjmat column
     adjmat_split = df['adjmat'].apply(lambda x: x.split('/'))
@@ -12,6 +16,28 @@ def extract_cols(df):
 
     return df
 
+def def_h(alg_path, res_path):
+    with open(alg_path, 'r') as f:
+        config = json.load(f)
+
+    df_defs = None
+
+    obj = config['resources']['structure_learning_algorithms']
+    for alg in obj:
+        try:
+            df = pd.read_csv(f'{os.path.join(res_path, alg)}.csv')
+        except:
+            continue
+
+        #print(alg)
+        params = list(obj[alg][0].keys())
+
+        df_def = best_all_alt(df, params)
+        df_def['id'] = df['id'].unique()[0]
+        df_defs = pd.concat([df_defs, df_def], ignore_index=True)
+    
+    return df_defs
+
 def _get_h(df):
     df = extract_cols(df)
     return df.groupby(['id', 'graph_p', 'graph_d', 'graph_type', 'data_sem', 'data_n', 'replicate'], as_index=False)
@@ -23,6 +49,14 @@ def best_h(df):
 def worst_h(df):
     df_h = _get_h(df)
     return df_h.apply(lambda x: x.loc[x['SHD_pattern'].idxmax()])
+
+def _get_m(df):
+    df = extract_cols(df)
+    return df.groupby(['graph_p', 'graph_d', 'graph_type', 'data_sem', 'data_n', 'replicate'], as_index=False)
+
+def best_m(df):
+    df_m = _get_m(df)
+    return df_m.apply(lambda x: x.loc[x['SHD_pattern'].idxmin()])
 
 def best_seeds(df, params):
     df = extract_cols(df)
@@ -62,3 +96,13 @@ def best_all(df, params):
     df3.rename(columns={'mean': 'SHD_mean', 'sem': 'SHD_sem'}, inplace=True)
 
     return df3
+
+def best_all_alt(df, params):
+    df = extract_cols(df)
+    df['hyper_id'] = df.groupby(params, dropna=False).ngroup()
+    gr = df.groupby(['hyper_id'])['SHD_pattern'].agg(['mean', 'sem']).reset_index()
+
+    hyper_id_sel = gr.loc[gr['mean'].idxmin(), 'hyper_id']
+    df['hyper_id_sel'] = hyper_id_sel
+
+    return df.loc[df['hyper_id'] == df['hyper_id_sel']]
