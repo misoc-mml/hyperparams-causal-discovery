4b5d8fe38adc93b329c3dbcbd2121de489e38895
dmachlanski <damian.machlanski@gmail.com>
2023-09-21 16:25:27 +0100

    Adding results of paper default settings.

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
-snakemake --cores all --use-singularity --configfile config/config_2.json
-
 #snakemake --cores all --use-singularity --latency-wait 20 --configfile config/hyper_sim_minimal.json
+
+#snakemake --cores all --use-singularity --latency-wait 20 --configfile config/hyper_sim_minimal_10k.json
+
+#snakemake --cores all --use-singularity --latency-wait 20 --configfile config/hyper_sim_minimal_default.json
 
 #snakemake --cores all --use-singularity --latency-wait 20 --configfile config/hyper_sachs.json
 
